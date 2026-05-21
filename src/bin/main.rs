709f76bd925b5b4e815802591cec01fced66d06f
Shay Howard-Bennett <shamus360@googlemail.com>
2026-05-21 13:54:24 +0100

    pulled out the led pwm into an embassy task.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,7 +8,9 @@
 #![deny(clippy::large_stack_frames)]
 
 use embassy_executor::Spawner;
-use embassy_time::Timer;
+
+use embassy_time::{Duration, Timer};
+use core::future::pending;
 
 use esp_hal::clock::CpuClock;
 use esp_hal::delay::Delay;
@@ -17,14 +19,13 @@ use esp_hal::gpio::{Level, Output, OutputConfig};
 use esp_hal::ledc::{
     LSGlobalClkSource, Ledc, LowSpeed, channel, channel::ChannelIFace, timer, timer::TimerIFace,
 };
-use esp_hal::timer::OneShotTimer;
+
 use esp_hal::timer::timg::TimerGroup;
 
 use esp_hal::analog::adc::{Adc, AdcConfig, Attenuation};
-use esp_hal::peripherals::{self, ADC1, GPIO12};
+use esp_hal::peripherals::{ ADC1,GPIO7,GPIO4, LEDC };
 use esp_println::println;
 
-use esp_hal::peripherals::GPIO7;
 use esp_hal::spi::{
     Mode,
     master::{Config, Spi},
@@ -32,7 +33,7 @@ use esp_hal::spi::{
 use esp_hal::time::Rate;
 #[allow(unused_imports)]
 use esp_radio::ble::controller::BleConnector;
-use log::{Level as LogLevel, error, info, log};
+use log::{error, info};
 
 use embedded_graphics::{
     mono_font::{MonoTextStyle, ascii::FONT_10X20},
@@ -49,6 +50,9 @@ fn panic(panic_info: &core::panic::PanicInfo) -> ! {
     error!("{}", panic_info);
     loop {}
 }
+
+
+use heapless::Vec;
 
 extern crate alloc;
 
@@ -70,6 +74,17 @@ async fn adc_read(adc1: ADC1<'static>, adc_pin: GPIO7<'static>) {
 
     let mut value;
 
+    //320px = 4095 adc output
+    //0px = 0 adc output
+    const GRAPH_Y_HEIGHT: u32 = 240;
+    let mut samples: Vec<u32, 320 > = Vec::new();
+    let mut graph_y_points: Vec<u32, 240> = Vec::new();
+
+    samples.push(2).unwrap();
+    samples.fill(0);
+
+    println!("length: {}",samples.len());
+
     loop {
         let mut total: u32 = 0;
 
@@ -83,90 +98,28 @@ async fn adc_read(adc1: ADC1<'static>, adc_pin: GPIO7<'static>) {
         }
 
         let avg = total / 32;
-        println!("{}", avg);
+        let normalised_adc = 1/avg;
+        // match graph_y_points.push(normalised_adc * GRAPH_Y_HEIGHT) {
+        //     Ok(_) => {},
+        //     Err(item) => {
+        //         //send all samples to drawing function
+        //         //clear samples
+
+        //     }
+        // }
+        // println!("{}", avg);
         Timer::after_millis(10).await;
     }
 }
 
-#[esp_rtos::main]
-async fn main(spawner: Spawner) -> ! {
-    // generator version: 1.3.0
-    // generator parameters: --chip esp32s3 -o unstable-hal -o alloc -o wifi -o ble-bleps -o embassy -o log
-
-    esp_println::logger::init_logger_from_env();
-
-    //configuration and initialisation of built in hardware
-    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let peripherals = esp_hal::init(config);
-
-    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 73744);
-    // COEX needs more RAM - so we've added some more
-    esp_alloc::heap_allocator!(size: 64 * 1024);
-
-    //embassy needs a hardware timer to schedule tasks
-    //it also uses a software interrupt to switch between tasks, the interrupt wakes it up.
-    //embassy is like axum or tokio.
-    let timg0 = TimerGroup::new(peripherals.TIMG0);
-
-    let sw_interrupt =
-        esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
-    esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
-
-    info!("Embassy initialized!");
-
-    ////initialise the wifi peripheral
-    // let (mut _wifi_controller, _interfaces) =
-    //     esp_radio::wifi::new(peripherals.WIFI, Default::default())
-    //         .expect("Failed to initialize Wi-Fi controller");
-    // let _connector = BleConnector::new(peripherals.BT, Default::default());
-
-    // TODO: Spawn some tasks
-    let task_spawner = spawner;
-
-    //pin definitions
-    //led
-    let led_pwm_pin = peripherals.GPIO4;
-
-    //LCD screen
-    let lcd_brightness_level_pin =
-        Output::new(peripherals.GPIO9, Level::High, OutputConfig::default());
-    let lcd_chip_select_pin = Output::new(peripherals.GPIO10, Level::High, OutputConfig::default());
-    let lcd_din_pin = peripherals.GPIO11;
-    let lcd_clock_pin = peripherals.GPIO12;
-    let lcd_reset_pin = Output::new(peripherals.GPIO13, Level::High, OutputConfig::default());
-    let lcd_data_command_pin = Output::new(peripherals.GPIO14, Level::Low, OutputConfig::default());
-    let mut delay = Delay::new();
-
-    //initialise peripheral
-    let mut spi = Spi::new(
-        peripherals.SPI2,
-        Config::default()
-            .with_frequency(Rate::from_mhz(1))
-            .with_mode(Mode::_0),
-    )
-    .unwrap()
-    .with_sck(lcd_clock_pin)
-    .with_mosi(lcd_din_pin);
-    //.with_cs(lcd_chip_select_pin);
-    //.with_miso(peripherals.GPIO2);
-
-    let spi_device = ExclusiveDevice::new_no_delay(spi, lcd_chip_select_pin).unwrap();
-    let mut buffer = [0_u8; 512];
-    let di = SpiInterface::new(spi_device, lcd_data_command_pin, &mut buffer);
-
-    let mut display = Builder::new(ST7789, di)
-        .display_size(240 as u16, 320 as u16)
-        .invert_colors(ColorInversion::Inverted)
-        .init(&mut delay)
-        .unwrap();
-
-    display.clear(Rgb565::BLUE).unwrap();
-
+#[embassy_executor::task]
+async fn fade_led(led_pwm_pin: GPIO4<'static>,ledc: LEDC<'static>){
+    
     //led PWM
     let led_pwm_pin = Output::new(led_pwm_pin, Level::High, OutputConfig::default());
 
     //get the singleton
-    let mut ledc = Ledc::new(peripherals.LEDC);
+    let mut ledc = Ledc::new(ledc);
 
     //use the singleton
     ledc.set_global_slow_clock(LSGlobalClkSource::APBClk);
@@ -189,9 +142,8 @@ async fn main(spawner: Spawner) -> ! {
         })
         .unwrap();
 
-    task_spawner.spawn(adc_read(peripherals.ADC1, peripherals.GPIO7).unwrap());
-
-    loop {
+    loop{
+        
         channel0.set_duty(0).unwrap();
         Timer::after_millis(1000).await;
         channel0.set_duty(20).unwrap();
@@ -204,5 +156,96 @@ async fn main(spawner: Spawner) -> ! {
         Timer::after_millis(1000).await;
         channel0.set_duty(100).unwrap();
         Timer::after_millis(1000).await;
+    
+    }
+}
+
+#[esp_rtos::main]
+async fn main(spawner: Spawner) -> ! {
+    // generator version: 1.3.0
+    // generator parameters: --chip esp32s3 -o unstable-hal -o alloc -o wifi -o ble-bleps -o embassy -o log
+
+    esp_println::logger::init_logger_from_env();
+
+    //configuration and initialisation of built in hardware
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 73744);
+    // COEX needs more RAM - so we've added some more
+    esp_alloc::heap_allocator!(size: 64 * 1024);
+
+
+    
+
+    //embassy needs a hardware timer to schedule tasks
+    //it also uses a software interrupt to switch between tasks, the interrupt wakes it up.
+    //embassy is like axum or tokio.
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+
+    let sw_interrupt =
+        esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+    esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
+
+    info!("Embassy initialized!");
+
+    ////initialise the wifi peripheral
+    // let (mut _wifi_controller, _interfaces) =
+    //     esp_radio::wifi::new(peripherals.WIFI, Default::default())
+    //         .expect("Failed to initialize Wi-Fi controller");
+    // let _connector = BleConnector::new(peripherals.BT, Default::default());
+
+    // TODO: Spawn some tasks
+    let task_spawner = spawner;
+
+    //pin definitions
+   
+    //LCD screen
+    let _lcd_brightness_level_pin = Output::new(peripherals.GPIO9, Level::High, OutputConfig::default());
+    let lcd_chip_select_pin = Output::new(peripherals.GPIO10, Level::High, OutputConfig::default());
+    let lcd_din_pin = peripherals.GPIO11;
+    let lcd_clock_pin = peripherals.GPIO12;
+    let _lcd_reset_pin = Output::new(peripherals.GPIO13, Level::High, OutputConfig::default());
+    let lcd_data_command_pin = Output::new(peripherals.GPIO14, Level::Low, OutputConfig::default());
+    let mut delay = Delay::new();
+
+    //initialise peripheral
+    let spi = Spi::new(
+        peripherals.SPI2,
+        Config::default()
+            .with_frequency(Rate::from_mhz(8))
+            .with_mode(Mode::_0),
+    )
+    .unwrap()
+    .with_sck(lcd_clock_pin)
+    .with_mosi(lcd_din_pin);
+
+
+    let spi_device = ExclusiveDevice::new_no_delay(spi, lcd_chip_select_pin).unwrap();
+    let mut buffer = [0_u8; 512];
+    let di = SpiInterface::new(spi_device, lcd_data_command_pin, &mut buffer);
+
+    const DISPLAY_WIDTH  :u32 = 320;
+    const DISPLAY_HEIGHT :u32 = 240;
+
+    let mut display = Builder::new(ST7789, di)
+        .display_size(DISPLAY_HEIGHT as u16, DISPLAY_WIDTH as u16)
+        .invert_colors(ColorInversion::Inverted)
+        .init(&mut delay)
+        .unwrap();
+    display.clear(Rgb565::CSS_PURPLE).unwrap();
+
+    
+
+
+    task_spawner.spawn(fade_led(peripherals.GPIO4, peripherals.LEDC).unwrap());
+    task_spawner.spawn(adc_read(peripherals.ADC1, peripherals.GPIO7).unwrap());
+
+    loop {
+    
+        Timer::after(Duration::from_millis(300)).await;
+      
+     
+        
     }
 }
